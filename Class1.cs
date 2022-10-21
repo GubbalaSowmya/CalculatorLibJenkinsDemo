e31754ecfc6ff5746863ae89ebd9081073381c43
Sowmya G <sowmyagubbala0681@gmail.com>
2022-10-21 10:56:33 +0530

    second commit

--- Class1.cs
+++ Class1.cs
@@ -9,7 +9,7 @@ namespace CalculatorLibJenkinsDemo
     public class Calculator
     {
         public int add(int i, int j) => i + j;
-        public int sub(int i, int j) => j - i;
+        public int sub( int j) => j - i;
         public int mul( int j) =>i * j;
 
     }
