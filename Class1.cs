72c774489aee58c3d1be67999e54118dbee31379
Sowmya G <sowmyagubbala0681@gmail.com>
2022-10-21 09:32:25 +0530

    Add project files.

---
+++ Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorLibJenkinsDemo
+{
+    public class Calculator
+    {
+        public int add(int i, int j) => i + j;
+        public int sub(int i, int j) => j - i;
+        public int mul(int i, int j) => i * j;
+
+    }
+}
